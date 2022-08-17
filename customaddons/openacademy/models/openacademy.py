b582e3e180e0490b99af997121954ca817234021
Henrique Paranhos <hparanhos@lliege.com.br>
2022-08-17 12:36:36 -0300

    Override Copy Method

--- customaddons/openacademy/models/openacademy.py
+++ customaddons/openacademy/models/openacademy.py
@@ -12,6 +12,29 @@ class Course(models.Model):
     responsible_id = fields.Many2one('res.users', ondelete='set null', string="Responsible", index=True)
     taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
 
+    def copy(self, default=None):
+        default = dict(default or {})
+
+        copied_count = self.search_count(
+            [('name', '=like', u"Copy of {}%".format(self.name))])
+        if not copied_count:
+            new_name = u"Copy of {}".format(self.name)
+        else:
+            new_name = u"Copy of {} ({})".format(self.name, copied_count)
+
+        default['name'] = new_name
+        return super(Course, self).copy(default)
+
+    _sql_constraints = [
+        ('name_description_check',
+         'check (name != description)',
+         'The course name and description can not be same.'),
+
+        ('course_name_unique',
+         'unique(name)',
+         'Course name should be unique'),
+    ]
+
     def _taken_sets(self):
         for r in self:
             if not r.seats:
